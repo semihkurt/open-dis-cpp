20b5aabff25c443ccfd0a00f91dc702b78a87506
Semih Kurt <semikurt@gmail.com>
2021-06-25 14:17:17 +0300

    pragma once for TransmitterPdu

--- src/dis7/TransmitterPdu.h
+++ src/dis7/TransmitterPdu.h
@@ -1,5 +1,4 @@
-#ifndef TRANSMITTERPDU_H
-#define TRANSMITTERPDU_H
+#pragma once
 
 #include <dis7/RadioType.h>
 #include <dis7/Vector3Double.h>
@@ -151,7 +150,6 @@ virtual int getMarshalledSize() const;
 };
 }
 
-#endif
 // Copyright (c) 1995-2009 held by the author(s).  All rights reserved.
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
